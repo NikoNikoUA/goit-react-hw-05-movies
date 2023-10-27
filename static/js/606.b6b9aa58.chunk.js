2ce0649040bd3a6672da82a7ba5cb3b8abdd98d1
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-10-27 13:20:55 +0000

    Deploying to gh-pages from @ NikoNikoUA/goit-react-hw-05-movies@cf9f1f5ad3fc7b41dc28e6086b11eca2e283f969 🚀

---
+++ static/js/606.b6b9aa58.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[606],{606:function(e,n,t){t.r(n),t.d(n,{default:function(){return u}});var o,r=t(87),a=t(168),c=t(867).ZP.div(o||(o=(0,a.Z)(["\n  color: red;\n  font-size: 18px;\n"]))),s=t(184),u=function(){return(0,s.jsxs)(c,{children:["Page is not found. Please go back to ",(0,s.jsx)(r.rU,{to:"/",children:"Home page"})]})}}}]);
+//# sourceMappingURL=606.b6b9aa58.chunk.js.map
